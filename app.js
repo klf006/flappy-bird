338cfa956d0e0663862a84e26345cc78ae18a160
klf006 <49726847+klf006@users.noreply.github.com>
2020-10-07 13:57:00 -0500

    fall and jump added

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid')
   const doodler = document.createElement('div')
   let doodlerLeftSpace = 50
-  let doodlerBottomSpace = 150
+  let doodlerBottomSpace = 250
   let isGameOver = false
   let platformCount = 5
+  let platforms = []
+  let upTimerId 
+  let downTimerId 
 
   function createDoodler() {
     grid.appendChild(doodler)
     doodler.classList.add('doodler')
     doodler.style.left = doodlerLeftSpace + 'px'
-    doodler.style.bottom = doodlerBottomSpace = 'px'
+    doodler.style.bottom = doodlerBottomSpace + 'px'
 
   }
 
@@ -33,14 +36,48 @@ document.addEventListener('DOMContentLoaded', () => {
       let platGap = 600 / platformCount
       let newPlatBottom = 100 + i * platGap
       let newPlatform = new Platform(newPlatBottom)
+      platforms.push(newPlatform)
     }
+  }
+
+  function movePlatforms() {
+    if (doodlerBottomSpace > 200) {
+      platforms.forEach(platform => {
+        platform.bottom -= 4
+        let visual = platform.visual 
+        visual.style.bottom = platform.bottom + 'px'
+      })
+    }
+  }
+
+  function jump() {
+    clearInterval(downTimerId)
+    upTimerId = setInterval(function () {
+      doodlerBottomSpace += 20
+      doodler.style.bottom = doodlerBottomSpace + 'px'
+      if (doodlerBottomSpace > 350) {
+        fall()
+      }
+    }, 30)
+  }
+
+  function fall() {
+    clearInterval(upTimerId)
+    downTimerId = setInterval(function () {
+      doodlerBottomSpace -= 5
+      doodler.style.bottom = doodlerBottomSpace + 'px'
+    }, 30)
   }
 
   function start() {
     if (!isGameOver) {
       createDoodler()
       createPlatforms()
+      setInterval(movePlatforms, 30)
+      jump()
     }
   }
+
+  //attach to button
   start()
 })
