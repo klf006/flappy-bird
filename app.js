8b126918d51d28603ec3bdaa168cfc083ce0f22a
klf006 <49726847+klf006@users.noreply.github.com>
2020-10-13 18:08:19 -0500

    finished functionality, needs styling

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ document.addEventListener('DOMContentLoaded', () => {
   let upTimerId 
   let downTimerId 
   let isJumping = true
+  let isGoingLeft = false
+  let isGoingRight = false
+  let leftTimerId 
+  let rightTimerId
+  let score = 0
 
   function createDoodler() {
     grid.appendChild(doodler)
@@ -49,6 +54,16 @@ document.addEventListener('DOMContentLoaded', () => {
         platform.bottom -= 4
         let visual = platform.visual 
         visual.style.bottom = platform.bottom + 'px'
+
+        if (platform.bottom < 10) {
+          let firstPlatform = platforms[0].visual
+          firstPlatform.classList.remove('platform')
+          platforms.shift()
+          score ++
+          console.log(platforms)
+          let newPlatform = new Platform(600)
+          platforms.push(newPlatform)
+        }
       })
     }
   }
@@ -93,18 +108,58 @@ document.addEventListener('DOMContentLoaded', () => {
   function gameOver() {
     console.log('game over')
     isGameOver = true
+    while (grid.firstChild) {
+      grid.removeChild(grid.firstChild)
+    }
+    grid.innerHTML = score
     clearInterval(upTimerId)
     clearInterval(downTimerId)
+    clearInterval(leftTimerId && rightTimerId)
   }
 
   function control(e) {
     if (e.key === "ArrowLeft") {
-      //move left
+      moveLeft()
     } else if (e.key === "ArrowRight") {
-      //move right
+      moveRight()
     } else if (e.key === "ArrowUp") {
-      //move Straight
+      moveStraight()
     }
+  }
+
+  function moveLeft() {
+    if(isGoingRight) {
+      clearInterval(rightTimerId)
+      isGoingRight = false
+    }
+    isGoingLeft = true
+    leftTimerId = setInterval(function () {
+      if(doodlerLeftSpace >= 0) {
+        doodlerLeftSpace -=5
+        doodler.style.left = doodlerLeftSpace + 'px' 
+      } else moveRight()
+    }, 25)
+  }
+
+  function moveRight() {
+    if (isGoingLeft) {
+      clearInterval(leftTimerId)
+      isGoingLeft = false
+    }
+    isGoingRight = true
+    rightTimerId = setInterval(function () {
+      if(doodlerLeftSpace <= 340) {
+        doodlerLeftSpace +=5
+        doodler.style.left = doodlerLeftSpace + 'px' 
+      } else moveLeft()
+    }, 25)
+  }
+
+  function moveStraight() {
+    isGoingLeft = false
+    isGoingRight = false
+    clearInterval(rightTimerId)
+    clearInterval(leftTimerId)
   }
 
   function start() {
@@ -113,6 +168,7 @@ document.addEventListener('DOMContentLoaded', () => {
       createDoodler()
       setInterval(movePlatforms, 30)
       jump()
+      document.addEventListener('keyup', control)
     }
   }
 
